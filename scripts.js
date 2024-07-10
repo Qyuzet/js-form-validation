48c986b9ccc27de5821b260d28a7f809540ca0e2
Qyuzet <riqyuzet@gmail.com>
2024-07-11 01:39:54 +0700

    V0.0.1

---
+++ scripts.js
@@ -0,0 +1,20 @@
+var nameError = document.getElementById("name-error");
+var phoneError = document.getElementById("phone-error");
+var emailError = document.getElementById("email-error");
+var messageError = document.getElementById("message-error");
+var submitError = document.getElementById("submit-error");
+
+function validateName() {
+  var name = document.getElementById("contact-name").value;
+  if (name.length == 0) {
+    nameError.innerHTML = "Name is required";
+    return false;
+  }
+
+  if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+    nameError.innerHTML = "write full name";
+    return false;
+  }
+  nameError.innerHTML = "valid";
+  return true;
+}
