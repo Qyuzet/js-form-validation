225ea183c77080ca569a81cc98d25f91bdd18100
Qyuzet <riqyuzet@gmail.com>
2024-07-11 23:25:13 +0700

    V0.0.1

--- scripts.js
+++ scripts.js
@@ -15,6 +15,72 @@ function validateName() {
     nameError.innerHTML = "write full name";
     return false;
   }
-  nameError.innerHTML = "valid";
+  nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
   return true;
+}
+
+function validatePhone() {
+  var phone = document.getElementById("contact-phone").value;
+
+  if (phone.length == 0) {
+    phoneError.innerHTML = "Phone no is required";
+    console.log("null value");
+    return false;
+  }
+  if (phone.length !== 12) {
+    phoneError.innerHTML = "Phone no should be 12 digits";
+    console.log("should 10");
+    return false;
+  }
+  if (!phone.match(/^[0-9]{12}$/)) {
+    phoneError.innerHTML = "Only digits please";
+    console.log("char");
+    return false;
+  }
+
+  phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+
+function validateEmail() {
+  var email = document.getElementById("contact-email").value;
+
+  if (email.length == 0) {
+    emailError.innerHTML = "Email is required";
+    return false;
+  }
+  if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    emailError.innerHTML = "Email Invalid";
+    return false;
+  }
+  emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+
+function validateMessage() {
+  var message = document.getElementById("contact-message").value;
+  var required = 30;
+  var left = required - message.length;
+  if (left > 0) {
+    messageError.innerHTML = left + " more character required";
+    return false;
+  }
+  messageError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+
+function validateForm() {
+  if (
+    !validateName() ||
+    !validatePhone() ||
+    !validateEmail() ||
+    !validateMessage()
+  ) {
+    submitError.style.display = "block";
+    submitError.innerHTML = "Please fix error to submit";
+    setTimeout(function () {
+      submitError.style.display = "none";
+    }, 3000);
+    return false;
+  }
 }
